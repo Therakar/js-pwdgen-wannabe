c0e56bf0f51a7b7e9b116c5f060d1e00d7be9f6c
Therakar <matteo.pau200@gmail.com>
2022-10-19 18:10:10 +0200

    Chiede il cognome all'utente

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ console.log(firstName);
 
 // output nella pagina del nome utente
 document.getElementById(`first-name`).innerHTML = firstName;
+
+// CHIEDE COGNOME ALL'UTENTE
+// input nome utente
+const lastName = prompt(`Inserisci il tuo cognome`);
+
+// output in console nome utente
+console.log(lastName);
+
+// output nella pagina del nome utente
+document.getElementById(`last-name`).innerHTML = lastName;
