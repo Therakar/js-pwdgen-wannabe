a32d0d1e9f120a6e80de7930057c660b9146a163
Therakar <matteo.pau200@gmail.com>
2022-10-19 18:14:32 +0200

    Chiede il colore preferito dell'utente

--- js/main.js
+++ js/main.js
@@ -8,12 +8,26 @@ console.log(firstName);
 // output nella pagina del nome utente
 document.getElementById(`first-name`).innerHTML = firstName;
 
+
+
 // CHIEDE COGNOME ALL'UTENTE
-// input nome utente
+// input cognome utente
 const lastName = prompt(`Inserisci il tuo cognome`);
 
-// output in console nome utente
+// output in console cognome utente
 console.log(lastName);
 
-// output nella pagina del nome utente
+// output nella pagina del cognome utente
 document.getElementById(`last-name`).innerHTML = lastName;
+
+
+
+// CHIEDE COLORE PREFERITO ALL'UTENTE
+// input colore preferito utente
+const favColor = prompt(`Inserisci il tuo colore preferito`);
+
+// output in console colore preferito utente
+console.log(favColor);
+
+// output nella pagina del colore preferito utente
+document.getElementById(`fav-color`).innerHTML = favColor;
